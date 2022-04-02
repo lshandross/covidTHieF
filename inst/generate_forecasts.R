7cb8628a1c12d456821eb8077027908877d8e0e7
lshandross <57642277+lshandross@users.noreply.github.com>
2022-04-02 11:47:27 -0400

    Update files to include 4 root models

--- inst/generate_forecasts.R
+++ inst/generate_forecasts.R
@@ -26,33 +26,31 @@ generate_thief_wk <-
 #    setwd("C:/Users/lshan/Documents/UMass Amherst/04 Senior/covidTHieF")
     func_list <- list.files(path = "R", pattern=".R", full.names=TRUE)
     lapply(func_list, source)
-    
+
         covid_thief(full_hosp_truth, "value",
           as.Date("2020-07-27"), fc_dates, # change as needed
           fips_vec = filter(hub_locations, geo_type == "state", population >= 500000) %>% pull(fips),
           aggregate_levels = c(84, 42, 28, 21, 14, 7, 1), frequency = 84, # change as needed
-          pi_levels = c(10 * (1:9), 95, 98))
+          pi_levels = c(10 * (1:9), 95, 98), transform.4root = TRUE) # change as needed
   }
-  
+
 # Export our function on the cluster
 clusterExport(cl, list('generate_thief_wk', 'full_hosp_truth', 'mon_fc_dates'))
 # Run function across previously specified number of cores
 system.time({
   thief_fc_full <- c(parLapply(cl, mon_fc_dates, fun = generate_thief_wk))
 })
- 
-for (i in 11:31) {
-  write_csv(thief_fc_full[[i]], file=paste("data/covidTHieF-Multiple3_arima_noTrans/", mon_fc_dates[i], "-covidTHieF-Multiple3_arima_noTrans.csv", sep=""))
+
+for (i in 25:31) {
+  write_csv(thief_fc_full[[i]], file=paste("data/Multiple3_arima_4root/", mon_fc_dates[i], "-covidTHieF-Multiple3_arima_4root.csv", sep=""))
 }
 
 
 
-forecast_list <- list.files(path = "data/covidTHieF-Multiple3_arima_noTrans/", pattern=".csv", full.names=TRUE)
-for (i in 18:31) {
-  df <- read_csv(forecast_list[i]) %>%
-    rename(target = "target_variable") %>%
-    select(forecast_date, target, target_end_date, location, type, quantile, value)
-  write_csv(df, file=paste("data/covidTHieF-Multiple3_arima_noTrans/", mon_fc_dates[i], "-covidTHieF-Multiple3_arima_noTrans.csv", sep=""))
+forecast_list <- list.files(path = "data/Base_arima_4root/", pattern=".csv", full.names=TRUE)
+for (i in 25:31) {
+  df <- read_csv(forecast_list[i])
+  write_csv(df, file=paste("data/Multiple3_arima_4root/", mon_fc_dates[i], "-covidTHieF-Multiple3_arima_4root.csv", sep=""))
 }
 getwd()
 forecast_list <- list.files(path = "data", pattern=".csv", full.names=TRUE)
