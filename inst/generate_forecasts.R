57d7c646895a7034c11fe9ab81a361430a7a0129
lshandross <57642277+lshandross@users.noreply.github.com>
2022-06-09 06:53:48 -0400

    organize separate functions and delete unnecessary code

--- inst/generate_forecasts.R
+++ inst/generate_forecasts.R
@@ -3,6 +3,7 @@ library(parallel)
 library(covidHubUtils)
 library(lubridate)
 library(readr)
+library(tidyverse)
 
 # returns number of available cores
 num_cores <- detectCores(logical=TRUE)
@@ -31,7 +32,7 @@ load_weekly_truth <- function(fc_dates) {
   df <- df %>%
     filter(target_end_date >= as.Date("2020-07-27"),
            geo_type == "state", population >= 500000) %>%
-    arrange(target_end_date, location)
+    arrange(desc(target_end_date), location)
 
   return(df)
 }
@@ -44,13 +45,17 @@ system.time({
   ttl <- c(parLapply(cl, sun_fc_dates, fun = load_weekly_truth))
 })
 
-save(training_truth_list, file="data/versioned_truth_training.RData")
+mon_training_truth_list <- training_truth_list
+sun_training_truth_list <- ttl
+
+save(mon_training_truth_list, sun_training_truth_list, file="data/versioned_truth_training.RData")
 load(file="data/versioned_truth_training.RData")
 
 #training_truth_df <- tibble(forecast_date=mon_fc_dates, training_truth_list)
 training_truth_df <- tibble(forecast_date=sun_fc_dates, ttl)
 
-# Create a function to process our data
+
+# Generate forecasts
 generate_thief_wk <-
   function(fc_dates) {
     library(tidyverse)
@@ -77,14 +82,14 @@ clusterExport(cl, list('generate_thief_wk', 'sun_fc_dates', 'training_truth_df')
 
 # Run function across previously specified number of cores
 system.time({
-  thief_fc_full <- c(parLapply(cl, sun_fc_dates[6:11], fun = generate_thief_wk))
+  thief_fc_full <- c(parLapply(cl, sun_fc_dates[6:9], fun = generate_thief_wk))
 })
 
 #models <- c("THieF_4wk-4root", "THieF_4wk-noTrans", "THieF_8wk-4root", "THieF_8wk-noTrans", "THieF_12wk-4root", "THieF_12wk-noTrans")
 
 #modfc_4wk_noTrans <- c()
 #modfc_8wk_4root <- c()
-for (i in 6:11) {
+for (i in 1:4) {
   write_csv(thief_fc_full[[i]][[1]], file=paste("data/", models[3], "/", mon_fc_dates[i+5], "-", models[3], ".csv", sep=""))
   modfc_8wk_4root <- rbind(modfc_8wk_4root, thief_fc_full[[i]][[2]])
 #  modfc_4wk_noTrans <- rbind(modfc_4wk_noTrans, thief_fc_full[[i]][[2]])
@@ -92,12 +97,3 @@ for (i in 6:11) {
 
 save(modfc_8wk_4root, file=paste("data/", models[3], "/", models[3], ".RData", sep=""))
 
-forecast_list <- list.files(path = "data/Base_arima_4root/", pattern=".csv", full.names=TRUE)
-for (i in 25:31) {
-  df <- read_csv(forecast_list[i])
-  write_csv(df, file=paste("data/Multiple3_arima_4root/", mon_fc_dates[i], "-covidTHieF-Multiple3_arima_4root.csv", sep=""))
-}
-getwd()
-forecast_list <- list.files(path = "data", pattern=".csv", full.names=TRUE)
-lapply(forecast_list, read_csv)
-read_csv(paste("data/", mon_fc_dates[28], "-covidTHieF-Topmost8_arima_noTrans.csv", sep=""))
