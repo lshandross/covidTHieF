d8ea2c65c9a3020728044006aa697f30bef554ad
lshandross <57642277+lshandross@users.noreply.github.com>
2022-06-09 07:47:47 -0400

    cleanup file and insert function to parallelize loading testing phase forecasts from other models

--- inst/generate_forecasts.R
+++ inst/generate_forecasts.R
@@ -16,8 +16,10 @@ registerDoParallel(cl)
 # full_hosp_truth <-
 #   load_truth("HealthData", "inc hosp", temporal_resolution="daily", data_location = "remote_hub_repo")
 
+# Date Vectors
 mon_fc_dates <- c(as.Date("2020-12-07") + weeks(0:29))
 sun_fc_dates <- c(as.Date("2020-12-06") + weeks(0:29))
+sun_testing_dates <- c(as.Date("2020-12-06") + weeks(0:29))
 
 # Save time by pre-loading as of truth data for all dates of interest in a list
 load_weekly_truth <- function(fc_dates) {
@@ -38,22 +40,18 @@ load_weekly_truth <- function(fc_dates) {
 }
 
 # Export our function on the cluster
-clusterExport(cl, list('load_weekly_truth', 'sun_fc_dates'))
+clusterExport(cl, list('load_weekly_truth', 'mon_fc_dates'))
 # Run function across previously specified number of cores
 system.time({
-#  training_truth_list <- c(parLapply(cl, mon_fc_dates, fun = load_weekly_truth))
-  ttl <- c(parLapply(cl, sun_fc_dates, fun = load_weekly_truth))
+  sun_training_truth_list <- c(parLapply(cl, sun_fc_dates, fun = load_weekly_truth))
 })
 
-mon_training_truth_list <- training_truth_list
-sun_training_truth_list <- ttl
-
-save(mon_training_truth_list, sun_training_truth_list, file="data/versioned_truth_training.RData")
+#save(mon_training_truth_list, sun_training_truth_list, file="data/versioned_truth_training.RData")
 load(file="data/versioned_truth_training.RData")
 
-#training_truth_df <- tibble(forecast_date=mon_fc_dates, training_truth_list)
-training_truth_df <- tibble(forecast_date=sun_fc_dates, ttl)
+training_truth_df <- tibble(forecast_date=sun_fc_dates, truth_data=sun_training_truth_list)
 
+states53 <- filter(hub_locations, geo_type == "state", population >= 500000) %>% pull(fips)
 
 # Generate forecasts
 generate_thief_wk <-
@@ -64,20 +62,20 @@ generate_thief_wk <-
 #    setwd("C:/Users/lshan/Documents/UMass Amherst/04 Senior/covidTHieF")
     func_list <- list.files(path = "R", pattern=".R", full.names=TRUE)
     lapply(func_list, source)
-    
+
     truth_df <- training_truth_df %>%
       filter(forecast_date == fc_dates) %>%
       pull(2) %>% pluck(1)
 
     covid_thief(truth_df, "value",
       as.Date("2020-07-27"), fc_dates, # change as needed
-      fips_vec = filter(hub_locations, geo_type == "state", population >= 500000) %>% pull(fips),
+      fips_vec = states53,
       aggregate_levels = list(56, 28, 14, 7, 1), frequency = 56, # change as needed
       pi_levels = c(10 * (1:9), 95, 98), transform.4root = TRUE) # change as needed
   }
 
 # Export our function on the cluster
-clusterExport(cl, list('generate_thief_wk', 'sun_fc_dates', 'training_truth_df'))
+clusterExport(cl, list('generate_thief_wk', 'states53', 'sun_fc_dates', 'training_truth_df'))
 #clusterExport(cl, list('generate_thief_wk', 'mon_fc_dates'))
 
 # Run function across previously specified number of cores
@@ -97,3 +95,24 @@ for (i in 1:4) {
 
 save(modfc_8wk_4root, file=paste("data/", models[3], "/", models[3], ".RData", sep=""))
 
+
+# Pull forecasts from other models
+#hub_models <- # eligible models
+pull_forecasts <- function(fc_dates) {
+  load_forecasts(#models = hub_models,
+                dates = fc_dates,
+                date_window_size = 6,
+                locations = states53,
+                types = c("point","quantile"),
+                targets = paste(0:30, "day ahead inc hosp"),
+                source = "zoltar",
+                verbose = FALSE,
+                as_of=NULL,
+                hub = c("US"))
+}
+
+system.time({
+  forecast_testing_list <- c(parLapply(cl, sun_testing_dates, fun = pull_forecasts))
+})
+
+save(forecast_testing_list, file=paste("data/", forecast_testing_list, ".RData", sep=""))
