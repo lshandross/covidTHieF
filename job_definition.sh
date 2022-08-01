db42ae9edbb4f79e3881502770cf71730c5d6edb
lshandross <57642277+lshandross@users.noreply.github.com>
2022-08-01 12:54:21 -0400

    create shell script to define cluster job

---
+++ job_definition.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#BSUB -n 4 # how many cores we want for our job
+#BSUB -R span[hosts=1] # ask for all the cores on a single machine
+#BSUB -R rusage[mem=6000] # ask for memory
+#BSUB -o job_log.out # log LSF output to a file
+#BSUB -W 12:00 # run time (hh:mm)
+#BSUB -q long # which queue we want to run in
+ 
+module load R/4.1.1
+module load xz/5.2.3
+module load binutils/2.37
+module load cmake/3.17.3
+module load bzip2/1.0.6_fPIC_lib
+module load pcre/8.40
+module load libtool/2.4.6
+
+R CMD BATCH --vanilla '--args linux 4 generate_forecasts sarima 1 47' inst/generate_forecasts.R logs/output.Rout
