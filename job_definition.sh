5db2087c7890c411cc98095829b3e58253f21002
Li Shandross [lshandross@umass.edu] <ls22a@ghpcc06.umassrc.org>
2022-08-02 15:34:19 -0400

    modify job definition shell script

--- job_definition.sh
+++ job_definition.sh
@@ -3,8 +3,8 @@
 #BSUB -R span[hosts=1] # ask for all the cores on a single machine
 #BSUB -R rusage[mem=6000] # ask for memory
 #BSUB -o job_log.out # log LSF output to a file
-#BSUB -W 12:00 # run time (hh:mm)
-#BSUB -q long # which queue we want to run in
+#BSUB -W 4:00 # run time (hh:mm)
+#BSUB -q short  # which queue we want to run in
  
 module load R/4.1.1
 module load xz/5.2.3
@@ -14,4 +14,4 @@ module load bzip2/1.0.6_fPIC_lib
 module load pcre/8.40
 module load libtool/2.4.6
 
-R CMD BATCH --vanilla '--args linux 4 generate_forecasts sarima 1 47' inst/generate_forecasts.R logs/output.Rout
+R CMD BATCH --vanilla '--args linux 8 generate_forecasts sarima 1 4' inst/generate_forecasts.R
