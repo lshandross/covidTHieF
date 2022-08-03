f61d057ed687535d5b3f418914524b6cd768d8a6
Li Shandross [lshandross@umass.edu] <ls22a@ghpcc06.umassrc.org>
2022-08-03 10:49:15 -0400

    update job_definition shell script

--- job_definition.sh
+++ job_definition.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BSUB -n 4 # how many cores we want for our job
+#BSUB -n 16 # how many cores we want for our job
 #BSUB -R span[hosts=1] # ask for all the cores on a single machine
 #BSUB -R rusage[mem=6000] # ask for memory
 #BSUB -o job_log.out # log LSF output to a file
@@ -14,4 +14,4 @@ module load bzip2/1.0.6_fPIC_lib
 module load pcre/8.40
 module load libtool/2.4.6
 
-R CMD BATCH --vanilla '--args linux 8 generate_forecasts sarima 1 4' inst/generate_forecasts.R
+R CMD BATCH --vanilla '--args linux 16 generate_forecasts sarima 1 47' inst/generate_forecasts.R
