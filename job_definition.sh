e2f387ad11dd1f49811c7dc9190c55d262357776
Li Shandross [lshandross@umass.edu] <ls22a@ghpcc06.umassrc.org>
2022-08-18 11:39:01 -0400

    update shell script

--- job_definition.sh
+++ job_definition.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#BSUB -n 32 # how many cores we want for our job
+#BSUB -n 64 # how many cores we want for our job
 #BSUB -R span[hosts=1] # ask for all the cores on a single machine
 #BSUB -R rusage[mem=1000] # ask for memory
 #BSUB -o job_log.out # log LSF output to a file
 #BSUB -W 4:00 # run time (hh:mm)
-#BSUB -q short  # which queue we want to run in
+#BSUB -q short # which queue we want to run in
  
 module load R/4.1.1
 module load xz/5.2.3
@@ -14,4 +14,4 @@ module load bzip2/1.0.6_fPIC_lib
 module load pcre/8.40
 module load libtool/2.4.6
 
-R CMD BATCH --vanilla '--args linux 32 generate_forecasts thief 1 T 1 47' inst/generate_forecasts.R
+R CMD BATCH --vanilla '--args linux 64 generate_forecasts thief 6 F 25 30' inst/generate_forecasts.R
