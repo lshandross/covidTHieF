8652db4f6e28b802f1d6150331c71a9fbffc2a04
Li Shandross [lshandross@umass.edu] <ls22a@ghpcc06.umassrc.org>
2022-10-25 13:18:06 -0400

    update shell script for ensemble job

--- job_ensemble.sh
+++ job_ensemble.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BSUB -n 64 # how many cores we want for our job
+#BSUB -n 32 # how many cores we want for our job
 #BSUB -R span[hosts=1] # ask for all the cores on a single machine
 #BSUB -R rusage[mem=1000] # ask for memory
 #BSUB -o job_log.out # log LSF output to a file
@@ -14,4 +14,4 @@ module load bzip2/1.0.6_fPIC_lib
 module load pcre/8.40
 module load libtool/2.4.6
 
-R CMD BATCH --vanilla '--args linux 64 generate_forecasts ensemble 6.5 NA 1 2' inst/generate_forecasts.R
+R CMD BATCH --vanilla '--args linux 32 generate_forecasts ensemble 6.5 F 1 25' inst/generate_forecasts.R
