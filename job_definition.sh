4e5291d96b32da597af3db555ec4432f35579534
lshandross <57642277+lshandross@users.noreply.github.com>
2022-11-12 14:38:24 -0500

    add phase argument to bash scripts

--- job_definition.sh
+++ job_definition.sh
@@ -15,4 +15,4 @@ module load bzip2/1.0.6_fPIC_lib
 module load pcre/8.40
 module load libtool/2.4.6
 
-R CMD BATCH --vanilla '--args linux 32 generate_forecasts thief 12 T 43 47' inst/generate_forecasts.R
+R CMD BATCH --vanilla '--args linux 32 generate_forecasts testing thief 6 T 1 12' inst/generate_forecasts.R
