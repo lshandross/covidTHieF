34cd156e9bf8268d6569e08c6b0824ebbaf37ca8
Li Shandross <57642277+lshandross@users.noreply.github.com>
2024-10-17 17:18:23 -0400

    Debug `compute_prob_forecasts()`

--- R/compute_prob_forecasts.R
+++ R/compute_prob_forecasts.R
@@ -16,18 +16,23 @@
 #'   each element in the provided hierarchy, with one sample per row.
 #' @export
 #'
-#' @examples
+#' @importFrom rlang .data
 compute_prob_forecasts <-
   function(temporal_hierarchy, n_samples = 10000, 
            forecast_type = c("base", "reconciled"), aggregate_levels, ...) {
+
+    # order temporal_hierarchy by frequency (top down)
+    agg_frequency <- sapply(temporal_hierarchy, function(ts) frequency(ts))
+    if (!identical(order(unname(agg_frequency)), seq_along(agg_frequency))) {
+      temporal_hierarchy <- temporal_hierarchy[order(agg_frequency)]
+    }
 
     fit <- lapply(
       temporal_hierarchy,
       function(x) forecast::auto.arima(ts(x, frequency = frequency(x)))
     )
-    fit_arima <- list(); for (i in 1:5) fit_arima[[i]] <- fit[[6-i]]
     forecast_obj <- lapply( # make into a forecast object
-      fit_arima,
+      fit,
       function(tsfit) forecast::forecast(tsfit, h = frequency(tsfit$x))
     )
 
@@ -35,39 +40,40 @@ compute_prob_forecasts <-
     res <- Reduce("c", sapply(forecast_obj, residuals, type = "response"))
     # in-sample residuals (one-step)
 
-    # Calculate probabilistic forecasts
-    base_forecasts <- MASS::mvrnorm(
-      n = n_samples, mu = unlist(base_mean), Sigma = FoReco::shrink_estim(mres)
-    )
-
-    if ("base" == forecast_type) {
-      return (base_forecasts)
-    } else if ("reconciled" %in% forecast_type) { # Gaussian reconciliation
-      # Multi-step residuals (approx base forecast error to inform reco weights)
-      hres_list <- lapply(
-        fit_arima,
-        function(mod) lapply(
+    # Multi-step residuals (approx base forecast error to inform reco weights)
+    hres_list <- lapply(
+      fit,
+      function(mod) {
+        lapply(
           1:frequency(mod$x), # get frequency of ts at each agg level
           function(h) residuals(mod, type = "response", h = h)
           # note res = y_t - yhat_t, where t < h (aka obs time before pred hzns)
         )
-        # list of residuals separated by aggregation level and time point
-      )
-      hres <- Reduce("c", lapply(hres_list, res2matrix::arrange_hres))
-      # Reduce list to vector (length 102) of residuals ordered like og data
-      # original data [102 total]: annual [6]; quarterly [24]; monthly [72]
+      } # list of residuals separated by aggregation level and time point
+    )
+    hres <- Reduce("c", lapply(hres_list, FoReco::arrange_hres))
+    # Reduce list to vector (length 102) of residuals ordered like og data
+    # original data [102 total]: annual [6]; quarterly [24]; monthly [72]
 
-      # Re-arrenge multi-step residuals in a matrix form
-      mres <- FoReco::res2matrix(hres, agg_order = aggregate_levels)
-      # m = 56, k* = 1 + 2 + 4 + 8 = 15, N = 336/56 = 6
-      # N(k* + m) = 6(71) = 426
+    # Re-arrenge multi-step residuals in a matrix form
+    mres <- FoReco::res2matrix(hres, agg_order = aggregate_levels)
+    # m = 56, k* = 1 + 2 + 4 + 8 = 15, N = 336/56 = 6
+    # N(k* + m) = 6(71) = 426
 
+    # Calculate probabilistic base forecasts
+    base_forecasts <- MASS::mvrnorm(
+      n = n_samples, mu = unlist(base_mean), Sigma = FoReco::shrink_estim(mres)
+    )
+
+    if (identical("base", forecast_type)) {
+      return (base_forecasts)
+    } else if ("reconciled" %in% forecast_type) { # Gaussian reconciliation
       reco_forecasts <- t(apply(
         base_forecasts, 1, FoReco::terec, agg_order = aggregate_levels,
-        comb = "wlsv", res = unlist(res), nn = "sntz"
+        res = unlist(res), ...#, comb = "wlsv", nn = "sntz"
       ))
 
-      if ("reconciled" == forecast_type) {
+      if (identical("reconciled", forecast_type)) {
         return(reco_forecasts)
       } else {
         return(list(base = base_forecasts, reconciled = reco_forecasts))
