0e3ee2052600b11ac7188dad6774e67a0a8df8de
Li Shandross <57642277+lshandross@users.noreply.github.com>
2024-12-03 16:04:51 -0500

    Add `transform_matrix_to_hub_df(keep_bottommost_only)` arg

--- R/transform_matrix_to_hub_df.R
+++ R/transform_matrix_to_hub_df.R
@@ -13,9 +13,12 @@
 #' @param quantile_levels Numeric vector of quantile levels (probabilities) to
 #'   calculate from the input `fc_matrix` sample forecasts. Defaults to
 #'   quartiles.
-#' @param h_ahead Number specifying the time units ahead of the longest horizon
+#' @param h_ahead Numeric specifying the time units ahead of the longest horizon
 #'   being forecast should be. Defaults to 56 with the assumption the unit is
 #'   in days (for a total of 8 weeks ahead).
+#' @param keep_bottommost_only Logical specifying whether to discard all
+#'   forecasts except for those from the bottommost level of the hierarchy.
+#'   Defaults to TRUE.
 #'
 #' @return A data frame of forecasts containing columns: \code{forecast_date},
 #' \code{location}, \code{target}, \code{target_end_date}, \code{type},
@@ -23,35 +26,41 @@
 #' sample forecasts.
 #' @export
 #'
-transform_matrix_to_hub_df <-
-  function(fc_matrix, forecast_date, fips_code, quantile_levels = c(0.25, 0.5, 0.75), h_ahead = 56) {
+#' @importFrom rlang .data
+transform_matrix_to_hub_df <- function(fc_matrix, forecast_date, fips_code,
+                                       quantile_levels = c(0.25, 0.5, 0.75),
+                                       h_ahead = 56,
+                                       keep_bottommost_only = TRUE) {
 
-    # extract quantiles - rows become quantile levels
-    fc_quantiles <- apply(fc_matrix, 2, stats::quantile, na.rm = TRUE,
-                          probs = quantile_levels)
+  # extract quantiles - rows become quantile levels
+  fc_quantiles <- apply(fc_matrix, 2, stats::quantile, na.rm = TRUE,
+                        probs = quantile_levels)
 
-    fc_quantiles |>
-      as.table() |>
-      as.data.frame() |>
-      tidyr::separate(
-        .data[["Var2"]], into = c("a", "k", "h"),
-        sep = "\\D+", convert = TRUE
-      ) |>
-      dplyr::mutate(
-        forecast_date = forecast_date,
-        location = fips_code,
-        horizon = as.numeric(.data[["h"]]) * .data[["k"]],
-        temporal_resolution = "daily",
-        target = "inc hosp",
-        target_end_date = forecast_date + .data[["horizon"]],
-        type = "quantile",
-        quantile = as.numeric(stringr::str_remove(.data[["Var1"]], "%")) * 0.01,
-        value = ifelse(.data[["Freq"]] < 0, 0, .data[["Freq"]]),
-      ) |>
-      dplyr::filter(
-        .data[["k"]] == 1, # keep only daily forecasts
-        .data[["horizon"]] <= h_ahead
-      ) |>
-      dplyr::select(c("forecast_date":"value")) |>
-      dplyr::tibble()
+  hub_df <- fc_quantiles |>
+    as.table() |>
+    as.data.frame() |>
+    tidyr::separate(
+      .data[["Var2"]], into = c("a", "k", "h"),
+      sep = "\\D+", convert = TRUE
+    ) |>
+    dplyr::mutate(
+      forecast_date = forecast_date,
+      location = fips_code,
+      horizon = as.numeric(.data[["h"]]) * .data[["k"]],
+      temporal_resolution = "daily",
+      target = "inc hosp",
+      target_end_date = forecast_date + .data[["horizon"]],
+      type = "quantile",
+      quantile = as.numeric(stringr::str_remove(.data[["Var1"]], "%")) * 0.01,
+      value = ifelse(.data[["Freq"]] < 0, 0, .data[["Freq"]]),
+    )
+
+  if (keep_bottommost_only) {
+    hub_df <- dplyr::filter(hub_df, .data[["k"]] == min(hub_df$k))
   }
+
+  hub_df |>
+    dplyr::filter(.data[["horizon"]] <= h_ahead) |>
+    dplyr::select(c("forecast_date":"value")) |>
+    dplyr::tibble()
+}
