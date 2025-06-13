3350cb5062659d1e42c509198854297e11a8383b
Li Shandross <57642277+lshandross@users.noreply.github.com>
2025-06-13 16:24:14 -0400

    Create covidTHieF-package.R

---
+++ R/covidTHieF-package.R
@@ -0,0 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom stats aggregate
+#' @importFrom stats frequency
+#' @importFrom stats median
+#' @importFrom stats quantile
+## usethis namespace: end
+NULL
